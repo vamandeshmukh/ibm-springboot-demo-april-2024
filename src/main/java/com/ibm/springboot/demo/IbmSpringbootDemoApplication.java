913310d1e0a627ff04a8c70f1b024278f9b57d7c
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-16 10:42:44 +0530

    spring boot intro

--- src/main/java/com/ibm/springboot/demo/IbmSpringbootDemoApplication.java
+++ src/main/java/com/ibm/springboot/demo/IbmSpringbootDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IbmSpringbootDemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Start");
 		SpringApplication.run(IbmSpringbootDemoApplication.class, args);
 	}
 
---
+++ src/main/java/com/ibm/springboot/demo/controller/Hello.java
@@ -0,0 +1,10 @@
+package com.ibm.springboot.demo.controller;
+
+public class Hello {
+
+	public String hi() {
+		System.out.println("Hello!");
+		return "Hello world!";
+	}
+
+}
