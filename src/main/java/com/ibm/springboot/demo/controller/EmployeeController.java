b6658b15e42608a9a7815d9b86df19d1e1a6398f
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-16 16:06:21 +0530

    first commit

--- src/main/java/com/ibm/springboot/demo/controller/EmployeeController.java
+++ src/main/java/com/ibm/springboot/demo/controller/EmployeeController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,15 +21,29 @@ public class EmployeeController {
 
 	@Autowired
 	EmployeeService employeeService;
-	
+
 //	http://localhost:8080/emp/get-emp-by-id/101
 
+//	@GetMapping("get-emp-by-id/{eid}")
+//	public ResponseEntity<Employee> getEmpById(@PathVariable(name = "eid") Integer employeeId) {
+//		System.out.println(employeeId);
+//		HttpStatus httpStatus = HttpStatus.OK;
+//		HttpHeaders httpHeaders = new HttpHeaders();
+//		httpHeaders.add("message", "Employee data fetched successfully.");
+//		Employee employee = employeeService.getEmployeeById(employeeId);
+//		return new ResponseEntity<Employee>(employee, httpHeaders, httpStatus);
+//	}
+
 	@GetMapping("get-emp-by-id/{eid}")
-	public Employee getEmpById( @PathVariable(name = "eid") Integer employeeId) {
+	public ResponseEntity<Employee> getEmpById(@PathVariable(name = "eid") Integer employeeId) {
 		System.out.println(employeeId);
 		Employee employee = employeeService.getEmployeeById(employeeId);
 		System.out.println(employee.toString());
-		return employee;
+		HttpStatus status = HttpStatus.OK;
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("message", "Employee data fetched successfully!");
+		ResponseEntity<Employee> response = new ResponseEntity<Employee>(employee, headers, status);
+		return response;
 	}
 
 	@GetMapping("get-all-emps")
@@ -37,6 +54,46 @@ public class EmployeeController {
 	}
 
 }
+
+//package com.ibm.springboot.demo.controller;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.ibm.springboot.demo.model.Employee;
+//import com.ibm.springboot.demo.service.EmployeeService;
+//
+//@RestController
+//@RequestMapping("emp")
+//public class EmployeeController {
+//
+//	@Autowired
+//	EmployeeService employeeService;
+//	
+////	http://localhost:8080/emp/get-emp-by-id/101
+//
+//	@GetMapping("get-emp-by-id/{eid}")
+//	public Employee getEmpById( @PathVariable(name = "eid") Integer employeeId) {
+//		System.out.println(employeeId);
+//		Employee employee = employeeService.getEmployeeById(employeeId);
+//		System.out.println(employee.toString());
+//		return employee;
+//	}
+//
+//	@GetMapping("get-all-emps")
+//	public List<Employee> getAllEmps() {
+//		List<Employee> empList = employeeService.getAllEmployees();
+//		empList.forEach(System.out::println);
+//		return empList;
+//	}
+//
+//}
 
 //package com.ibm.springboot.demo.controller;
 //
