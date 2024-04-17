c3e04e44162f52416e84d4d69b55a0484b84ecc9
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-17 10:54:01 +0530

    springboot

--- src/main/java/com/ibm/springboot/demo/App.java
+++ src/main/java/com/ibm/springboot/demo/App.java
@@ -1,12 +1,14 @@
 package com.ibm.springboot.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
-	
-	LOG 
+
+	private static final Logger LOG = LoggerFactory.getLogger(App.class);
 
 	public static void main(String[] args) {
 		LOG.info("Starting...");
---
+++ src/main/java/com/ibm/springboot/demo/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.ibm.springboot.demo.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository {
+
+}
