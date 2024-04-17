32f33413aa24589ba896090bfec6d28c8143d745
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-17 09:37:04 +0530

    springboot

--- src/main/java/com/ibm/springboot/demo/service/EmployeeServiceImpl.java
+++ src/main/java/com/ibm/springboot/demo/service/EmployeeServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.ibm.springboot.demo.model.Employee;
@@ -11,13 +13,17 @@ import com.ibm.springboot.demo.model.Employee;
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
+	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+
 	private List<Employee> empList = new ArrayList<>(
 			Arrays.asList(new Employee(101, "Sonu", (double) 90000), new Employee(102, "Monu", (double) 99000),
 					new Employee(103, "Tonu", (double) 2000), new Employee(104, "Gonu", (double) 89000)));
 
 	@Override
 	public Employee getEmployeeById(Integer employeeId) {
-		System.out.println(employeeId);
+		LOG.info(employeeId.toString());
+		LOG.warn(employeeId.toString());
+		LOG.error(employeeId.toString());
 		return empList.stream().filter(emp -> emp.getEmployeeId().equals(employeeId)).findFirst().orElse(null);
 	}
 
