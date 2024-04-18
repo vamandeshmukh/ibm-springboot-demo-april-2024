acfccf20de7b88715de80dad4153744252199ba9
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-18 15:27:00 +0530

    spring boot 3

--- src/main/java/com/ibm/springboot/demo/aspect/EmployeeServiceAspect.java
+++ src/main/java/com/ibm/springboot/demo/aspect/EmployeeServiceAspect.java
@@ -1,16 +1,27 @@
 package com.ibm.springboot.demo.aspect;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+@Aspect
+@Component
 public class EmployeeServiceAspect {
 
 	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
 
+	@Before("execution(* com.ibm.springboot.demo.service.*.*(..))")
 	public void serviceMethods() {
 		LOG.info("a method from EmployeeServiceImpl was invoked.");
 	}
 
+	@After("execution(* com.ibm.springboot.demo.service.*.*(..))")
+	public void serviceMethods2() {
+		LOG.info("After a method from EmployeeServiceImpl was invoked.");
+	}
 }
 
 //package com.ibm.springboot.demo.aspect;
