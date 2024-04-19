76586062b493878f6fd3d1abbd624a29839b1aac
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-19 15:35:48 +0530

    spring boot

--- src/main/java/com/ibm/springboot/demo/model/Department.java
+++ src/main/java/com/ibm/springboot/demo/model/Department.java
@@ -3,7 +3,20 @@ package com.ibm.springboot.demo.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Document("departments")
+//@Getter
+//@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Department {
 
 	@Id
@@ -13,5 +26,4 @@ public class Department {
 	private String managerId;
 
 	// constructors, getters, setters, toString etc
-
 }
