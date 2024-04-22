8e2446cdaf201212e0a3eb9d34ee4dd16ef9dabd
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-22 12:12:57 +0530

    commit

--- src/test/java/com/ibm/springboot/demo/service/EmployeeServiceTests.java
+++ src/test/java/com/ibm/springboot/demo/service/EmployeeServiceTests.java
@@ -1,10 +1,15 @@
 package com.ibm.springboot.demo.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -34,11 +39,34 @@ public class EmployeeServiceTests {
 		empList.add(new Employee());
 		empList.add(new Employee());
 
+//		Mockito.when(employeeRepository.findAll()).thenReturn(empList);
 		when(employeeRepository.findAll()).thenReturn(empList);
+	}
+
+	@BeforeAll
+	public static void beforeAllStuff() {
+
+	}
+
+	@AfterAll
+	public static void afterAllStuff() {
+
 	}
 
 	@Test
 	public void testAllEmps() {
 		assertEquals(employeeService.getAllEmployees().size(), 3);
+	}
+
+	@Test
+	public void testAllEmps2() {
+		assertNotEquals(employeeService.getAllEmployees().size(), 4);
+	}
+
+	@Test
+	void testAllEmpsTimes() {
+		employeeRepository.findAll();
+		employeeRepository.findAll();
+		verify(employeeRepository, times(2)).findAll();
 	}
 }
