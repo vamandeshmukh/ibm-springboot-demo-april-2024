b974dc3dfd6c055bc0971c90cb4ba4e4dcbe2cd7
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-22 14:50:31 +0530

    commit

--- src/test/java/com/ibm/springboot/demo/controller/EmployerControllerTests.java
+++ src/test/java/com/ibm/springboot/demo/controller/EmployerControllerTests.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.autoconfigure.wavefront.WavefrontProperties.Application;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
